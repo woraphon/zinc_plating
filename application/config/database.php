4d31538e2c41da78f507391cd970a05a3bfc77cb
woraphon supharp <woraphon.sx@gmail.com>
2015-04-27 17:44:25 +0700

    เปลียน database
    เอาการเชื่อมต่อบน server ออก

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'design2hou_api';
-$db['default']['password'] = 'roman015';
-$db['default']['database'] = 'design2hou_api';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
