915837d9eb3d77129341576acbd41838e11d5131
woraphon supharp <woraphon.sx@gmail.com>
2015-04-29 15:06:03 +0700

    update

--- application/views/page/home/home.php
+++ application/views/page/home/home.php
@@ -4,19 +4,22 @@
    //$this->view('include/slide');
 ?>   
 	<div class="container">
-	<?php $this->view('include/slide');?>
-<div class="row">
 
+<div class="row">
+	<br>
+		<div class="col-md-6"> 
+			<?php $this->view('include/slide');?>	
+		</div>
 		<!-- main content -->
-		<hr>
-      	<div class="col-md-12">
+		
+      	<div class="col-md-6">
       	<!-- open Update -->
       	<div class="panel panel-black">
- 		 <!-- Default panel contents -->
-  		<div class="panel-heading"></div>
+ 		 <!-- Default panel contents 
+  		<div class="panel-heading"></div> -->
   		<div class="panel-body">
         		<div class="row">
-  					<div class="col-sm-6 col-md-3">
+  					<div class="col-sm-6 col-md-6">
    	 					<div class="boxpic">
       						<img src="<?php echo base_url()?>images/home/jig.jpg" alt="...">
       						<div class="caption">
@@ -25,7 +28,7 @@
       						</div>
     					</div>
   					</div>
-  					<div class="col-sm-6 col-md-3">
+  					<div class="col-sm-6 col-md-6">
    	 					<div class="boxpic">
       						<img src="<?php echo base_url()?>images/home/zinc.jpg" alt="...">
       						<div class="caption">
@@ -34,68 +37,17 @@
       						</div>
     					</div>
   					</div>
-  					<div class="col-sm-6 col-md-3">
-   	 					<div class="boxpic">
-      						<img src="<?php echo base_url()?>images/home/chromium.jpg" alt="...">
-      						<div class="caption">
-        						<!-- <h3 align="center">CHROMIUM</h3> -->
-        						<p align="center"><a href="#" class="btn btn-primary btn-sm" role="button">CHROMIUM</a> </p>
-      						</div>
-    					</div>
-  					</div>
-  					<div class="col-sm-6 col-md-3">
-   	 					<div class="boxpic">
-      						<img src="<?php echo base_url()?>images/home/nickel.jpg" alt="...">
-      						<div class="caption">
-        						<!-- <h3 align="center">NICKEL</h3> -->
-        						<p align="center"><a href="#" class="btn btn-primary btn-sm" role="button">NICKEL</a> </p>
-      						</div>
-    					</div>
-  					</div>
-  					<div class="col-sm-6 col-md-3">
-   	 					<div class="boxpic">
-      						<img src="<?php echo base_url()?>images/home/silver.jpg" alt="...">
-      						<div class="caption">
-        						<!-- <h3 align="center">SILVER</h3> -->
-        						<p align="center"><a href="#" class="btn btn-primary btn-sm" role="button">SILVER</a> </p>
-      						</div>
-    					</div>
-  					</div>
-  					<div class="col-sm-6 col-md-3">
-   	 					<div class="boxpic">
-      						<img src="<?php echo base_url()?>images/home/copper.jpg" alt="...">
-      						<div class="caption">
-        						<!-- <h3 align="center">COPPER</h3> -->
-        						<p align="center"><a href="#" class="btn btn-primary btn-sm" role="button">COPPER</a> </p>
-      						</div>
-    					</div>
-  					</div>
-  					<div class="col-sm-6 col-md-3">
-   	 					<div class="boxpic">
-      						<img src="<?php echo base_url()?>images/home/black-nickel.jpg" alt="...">
-      						<div class="caption">
-        						<!-- <h3 align="center">BLACK NICKEL</h3> -->
-        						<p align="center"><a href="#" class="btn btn-primary btn-sm" role="button">BLACK NICKEL</a> </p>
-      						</div>
-    					</div>
-  					</div>
-  					<div class="col-sm-6 col-md-3">
-   	 					<div class="boxpic">
-      						<img src="<?php echo base_url()?>images/home/project.jpg" alt="...">
-      						<div class="caption">
-        						<!-- <h3 align="center">PROJECT</h3> -->
-        						<p align="center"><a href="#" class="btn btn-primary btn-sm" role="button">PROJECT</a> </p>
-      						</div>
-    					</div>
-  					</div>
+  					
   					
 				</div>
 			</div>
 			</div>
 			<!-- close update  -->	
   			</div>
+  		
       	</div>
-	
+      
+		
 
    
    
